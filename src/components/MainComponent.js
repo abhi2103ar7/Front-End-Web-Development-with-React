62395c0b6c34012d3f12347e543763583e46157e
Abhishek <abhishekranjan@karunya.edu.in>
2020-06-12 22:54:12 +0530

    Assignment 2(about Page)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from "./DishdetailComponent";
@@ -47,6 +48,7 @@ class Main extends Component{
         <Header />
         <Switch>
           <Route path="/home" component={HomePage}/>
+          <Route path='/aboutus' component={() => <About leader= {this.state.leaders}></About>} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
           <Route path="/menu/:dishId" component={DishWithId}/>
           <Route exact path="/contactus" component={()=><Contact/>}/>
